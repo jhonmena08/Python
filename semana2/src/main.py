0a7b2e3cd55bee64d9f93f324a797531d24395ec
jhonmena08 <jhonmenahurtado@gmail.com>
2025-10-09 17:36:28 -0500

    paquete pandas

--- semana2/src/main.py
+++ semana2/src/main.py
@@ -3,20 +3,21 @@ import pandas as pd
 def main():
     df = pd.read_csv('C:\\Users\\Jhonm\\repos\\curso_python\\semana2\\cars2_raw.csv')
 
-    # convertir a int a float
-    df['price'] = df['price'].astype(float)
-
-    # Opción para ajustar al contenido (autoajuste)
-    pd.set_option('display.max_colwidth', None)
+    # Opción para ajustar ancho columnas (20 caracteres)
+    pd.set_option('display.max_colwidth', 20)
 
     # Establecer el formato de visualización floats
     pd.options.display.float_format = '${:,.0f}'.format
+
+    # convertir a int a float
+    df['price'] = df['price'].astype(float)
+
 
     # filtrar
     # df2 = df[df['price'] == df['price'].max()]
 
     print()
-    print(df[['product', 'price', 'kilometraje']])
+    print(df)
     print()
 
 
