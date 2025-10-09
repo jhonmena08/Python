4eade0a932e6be9a18c4c0cf8f446a4ae570ba66
jhonmena08 <jhonmenahurtado@gmail.com>
2025-10-09 17:55:29 -0500

    vehiculos menos 20 millones

--- semana2/src/main.py
+++ semana2/src/main.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import os
 
+
 df = pd.read_csv('C:\\Users\\Jhonm\\repos\\curso_python\\semana2\\cars2_raw.csv')
 pd.set_option('display.max_colwidth', 15)
-# pd.options.display.float_format = '${:,.0f}'.format
 
 
 def main() -> int:
     os.system('cls')
 
     # filtrar
-    df2 = df[df['price'] == df['price'].max()]
+    df2 = df[df['price'] < 20_000_000]
     print(df2)
 
     print()
