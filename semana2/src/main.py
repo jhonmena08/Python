b49f8ed162fcdddde0803cc5f02ba994c47a4e4a
jhonmena08 <jhonmenahurtado@gmail.com>
2025-10-09 17:44:19 -0500

    filatra por precio vehiculo

--- semana2/src/main.py
+++ semana2/src/main.py
@@ -1,24 +1,20 @@
 import pandas as pd
+import os
 
-def main():
-    df = pd.read_csv('C:\\Users\\Jhonm\\repos\\curso_python\\semana2\\cars2_raw.csv')
+df = pd.read_csv('C:\\Users\\Jhonm\\repos\\curso_python\\semana2\\cars2_raw.csv')
+pd.set_option('display.max_colwidth', 15)
+# pd.options.display.float_format = '${:,.0f}'.format
 
-    # Opción para ajustar ancho columnas (20 caracteres)
-    pd.set_option('display.max_colwidth', 20)
 
-    # Establecer el formato de visualización floats
-    pd.options.display.float_format = '${:,.0f}'.format
-
-    # convertir a int a float
-    df['price'] = df['price'].astype(float)
-
+def main() -> int:
+    os.system('cls')
 
     # filtrar
-    # df2 = df[df['price'] == df['price'].max()]
+    df2 = df[df['price'] == df['price'].max()]
+    print(df2)
 
     print()
-    print(df)
-    print()
+    return 0
 
 
 if __name__ == "__main__":
