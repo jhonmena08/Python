b0b154e2e522c537be57c8f130a45d380d2419ff
jhonmena08 <jhonmenahurtado@gmail.com>
2025-10-09 11:05:59 -0500

    usando filtros paquete pandas

--- semana2/src/main.py
+++ semana2/src/main.py
@@ -2,7 +2,19 @@ import pandas as pd
 
 def main():
     df = pd.read_csv('C:\\Users\\Jhonm\\repos\\curso_python\\semana2\\cars2_raw.csv')
-    print(df[['product', 'price']].head(5))
+
+    # convertir a int a float
+    df['price'] = df['price'].astype(float)
+
+    # Establecer el formato de visualización floats
+    pd.options.display.float_format = '${:,.0f}'.format
+
+    # filtrar
+    # df2 = df[df['price'] == df['price'].max()]
+
+    print()
+    print(df[df['price'] == df['price'].min()][['product', 'price', 'kilometraje']])
+    print()
 
 
 if __name__ == "__main__":
