a48b0e3e4bcb37f0c4b2d16d7982b979282692b5
jhonmena08 <jhonmenahurtado@gmail.com>
2025-10-09 19:57:57 -0500

    clase 09-octubre

--- semana2/src/main.py
+++ semana2/src/main.py
@@ -1,19 +1,44 @@
 import pandas as pd
 import os
 
-
-df = pd.read_csv('C:\\Users\\Jhonm\\repos\\curso_python\\semana2\\cars2_raw.csv')
+# configuraciones
 pd.set_option('display.max_colwidth', 15)
+
+
+def get_bronzen_data() -> pd.DataFrame:
+    return pd.read_csv('C:\\Users\\Jhonm\\repos\\curso_python\\semana2\\cars2_raw.csv')
+
+
+def write_gold_data(data: pd.DataFrame) -> None:
+    data.to_excel('C:\\Users\\Jhonm\\repos\\curso_python\\semana2\\cars2_gold.xlsx')
+
+
+def convert_date(data: pd.DataFrame) -> pd.DataFrame:
+    # corregir las fechas al formato 2025-10-28
+    data['created'] = data['created'].map(lambda x: x[:10])
+    return data
+
+
+def add_columns(data: pd.DataFrame) -> pd.DataFrame:
+    # agregar dos columnas para marca y linea del vehiculo
+    data['marca'] = data['product'].str.split(' ').str[0]
+    data['linea'] = data['product'].str.split(' ').str[1]
+    return data
+
 
 
 def main() -> int:
     os.system('cls')
 
-    # filtrar
-    df2 = df[df['price'] < 20_000_000][['product', 'price', 'created']]
-    print(df2)
+    print('Iniciando procesamiento ..')
+    df = get_bronzen_data()
+    convert_date(df)
+    add_columns(df)
 
-    print()
+    write_gold_data(df)
+    print(df)
+
+
     return 0
 
 
